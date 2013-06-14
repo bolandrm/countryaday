c7ad077a40eb54a456e3ce305a81392b54559e30
Ryan Boland <bolandryanm@gmail.com>
2013-06-14 19:02:43 -0400

    migrate countries_countroller from coffe

--- client/app/js/app2.js
+++ client/app/js/app2.js
@@ -1,14 +1,3 @@
-countries.controller('CountriesController', [
-  '$rootScope', '$scope', '$routeParams', 'MapValues', 'Country', 'summary', 'info', 'news', function($rootScope, $scope, $routeParams, MapValues, Country, summary, info, news) {
-    $scope.country = $routeParams.country;
-    $scope.code = Country.fromName($scope.country).code;
-    MapValues.setCurrent($scope.code);
-    $scope.info = info;
-    $scope.news = news;
-    return $scope.paragraphs = summary;
-  }
-]);
-
 countries.filter('expandCurrency', function() {
   var currencyCodes;
 
