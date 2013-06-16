5951e9348967242d426c091bf8eab612b424eae3
Ryan Boland <bolandryanm@gmail.com>
2013-06-16 19:12:10 -0400

    setup rspec testing

---
+++ server/spec/spec_helper.rb
@@ -0,0 +1,13 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../app.rb'
+require 'rspec'
+require 'rack/test'
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
+
+def app
+  CountryADay 
+end
