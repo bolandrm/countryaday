e7ee9284c806cf5dcf5e1e5ed088ee4fc0de04ad
Ryan Boland <bolandryanm@gmail.com>
2013-06-30 13:34:57 -0400

    split integration specs

--- spec/integration/countries_spec.rb
+++ spec/integration/countries_spec.rb
@@ -1,36 +1,5 @@
 require_relative 'integration_helper'
 
-describe 'registration' do
-  it 'creates user' do
-    expect { visit '/auth/facebook/callback' }.to change{ User.all.count }.by(1)
-  end
-
-  it 'sets current country to their first country' do
-    Capybara.current_session.driver.browser.set_cookie 'firstCountry=US'
-    visit '/auth/facebook/callback'
-    expect(passed_data('current-country')).to eq('US')
-  end
-
-  it 'randomizes current country when a first country has NOT been selected' do
-    visit '/auth/facebook/callback'
-    expect(passed_data('current-country').length).to eq(2)
-  end
-end
-
-describe 'authentication' do
-  before { signup }
-
-  it 'allows users to sign out' do
-    visit '/'
-    expect(passed_data('signed-in')).to eq('false')
-  end
-
-  it 'allows users to sign in' do
-    expect { signin }.to_not change { User.all.count }
-    expect(passed_data('signed-in')).to eq('true')
-  end
-end
-
 describe 'countries' do
   before { signin }
 
@@ -68,15 +37,3 @@ describe 'countries' do
   end
 end
 
-def signup
-  signin
-  visit '/signout'
-end
-
-def signin
-  visit '/auth/facebook/callback'
-end
-
-def passed_data(attribute)
-  find('body')["data-#{attribute}"]
-end
--- spec/integration/integration_helper.rb
+++ spec/integration/integration_helper.rb
@@ -10,3 +10,17 @@ ActiveRecord::Base.logger.level = Logger::INFO
 RSpec.configure do |config|
   config.include Capybara::DSL
 end
+
+
+def signup
+  signin
+  visit '/signout'
+end
+
+def signin
+  visit '/auth/facebook/callback'
+end
+
+def passed_data(attribute)
+  find('body')["data-#{attribute}"]
+end
