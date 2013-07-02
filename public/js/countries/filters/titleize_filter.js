f1d7eef2cb2eb792c7386510317dab856fd49f50
Ryan Boland <bolandryanm@gmail.com>
2013-07-02 18:44:43 -0400

    handle undefined case for titleize filter

--- public/js/countries/filters/titleize_filter.js
+++ public/js/countries/filters/titleize_filter.js
@@ -1,5 +1,6 @@
 countries.filter('titleize', function() {
   return function(text) {
+    if (!text) { return ''; }
     return text.replace(/_/g, ' ').replace(/\w\S*/g, function(txt) {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
